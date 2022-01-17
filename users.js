53039741d6148d73760e1e99535d0840b27d1707
Ruan Pedro Mendes <72527894+Ruan-Pedro@users.noreply.github.com>
2022-01-16 22:32:05 -0300

    Add files via upload

---
+++ users.js
@@ -0,0 +1,21 @@
+ users = [
+  // {
+  //   name: 'Ruan Pedro',
+  //   email: 'ruanpmp@gmail.com',
+  //   text: 'Texto de teste'
+  // },
+  // {
+  //   name: 'ruandev',
+  //   email: 'ruandev021@gmail.com',
+  //   text: 'teste do texto de um xablau'
+  // },
+    // "ruandev021@gmail.com",
+    // "ruanpmp@gmail.com",
+    // "brunobinocencio@gmail.com"
+    // "stefania.moreno4269@gmail.com",
+    // "Mauricio.strato2@gmail.com",
+    // "mauricio.strato2@gmail.com"
+    // "alziranazaresud@gmail.com"
+  ] /*,"elaine97carol@gmail.com","victoriamiola@gmail.com","lucascosta.lc1@gmail.com","guastavocruz@gmail.com","beatrizferim@gmail.com","nathalyabrasil5@gmail.com"], */,
+  
+  module.exports = users
